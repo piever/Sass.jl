2de5706d0249de70aaf36f594dc1fd7c8db83113
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-10-10 11:41:16 +0100

    add output_style

--- src/options.jl
+++ src/options.jl
@@ -1,14 +1,15 @@
-export sass_option_get_precision, sass_option_set_precision
+export sass_option_get_output_style, sass_option_set_output_style
 export sass_option_get_source_comments, sass_option_set_source_comments
+export sass_option_get_precision, sass_option_set_precision
 
-function sass_option_get_precision(options)
-    ccall((:sass_option_get_precision, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
+function sass_option_get_output_style(options)
+    ccall((:sass_option_get_output_style, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
         Cint, (Ptr{Cvoid},), options)
 end
 
-function sass_option_set_precision(options, precision)
-    ccall((:sass_option_set_precision, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
-        Cvoid, (Ptr{Cvoid}, Cint), options, precision)
+function sass_option_set_output_style(options, output_style)
+    ccall((:sass_option_set_output_style, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
+        Cvoid, (Ptr{Cvoid}, Cint), options, output_style)
 end
 
 function sass_option_get_source_comments(options)
@@ -19,4 +20,14 @@ end
 function sass_option_set_source_comments(options, comment)
     ccall((:sass_option_set_source_comments, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
         Cvoid, (Ptr{Cvoid}, Cint), options, comment)
+end
+
+function sass_option_get_precision(options)
+    ccall((:sass_option_get_precision, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
+        Cint, (Ptr{Cvoid},), options)
+end
+
+function sass_option_set_precision(options, precision)
+    ccall((:sass_option_set_precision, "/home/pietro/.julia/dev/Sass/deps/lib/libsass.so"),
+        Cvoid, (Ptr{Cvoid}, Cint), options, precision)
 end
