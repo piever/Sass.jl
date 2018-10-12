5dcb6b54aabe13756fe77387b15e9853c82e5447
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-10-12 12:57:20 +0100

    revert debugging statements

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,6 @@
-println("Before running test!")
 using Sass
-println("Precompiled successfully!")
 using Test
 
-filename = joinpath(Sass.examplefolder, "test.sass")
-println("Defined filename")
-css = Sass.compile_file(filename; output_style = Sass.nested)
-println("Compiled file!")
 @testset "outputstyle" begin
     css = Sass.compile_file(filename; output_style = Sass.nested)
     @test css == "body {\n  font: 100% Helvetica, sans-serif;\n  color: #333; }\n"
